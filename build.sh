1b276949a485f1332762ea7a88aa51d6ac26391f
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-08-15 18:48:59 -0400

    game window and fps

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 export VCPKG_ROOT=~/dev/tools/vcpkg
 
-cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake
-cmake --build build
+cmake -B si -S . -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake
+cmake --build si
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,23 @@
 #include "raylib.h"
 
 int main() {
-    InitWindow(800, 450, "raylib [core] example - basic window");
 
-    while (!WindowShouldClose())
-    {
+    int windowWidth = 750;
+    int windowHeight = 700;
+
+    // Game window
+    InitWindow(windowWidth, windowHeight, "C++ - Space Invaders");
+    // Game FPS
+    SetTargetFPS(60);
+
+    // Game loop. Runs will the window is not closed
+    while (!WindowShouldClose()) {
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            ClearBackground(Color{255, 255, 255, 255});
         EndDrawing();
     }
 
+    // Close our game window
     CloseWindow();
     return 0;
 }
